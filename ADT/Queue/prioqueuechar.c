4b449780d566f29d30c1e1dbb026fe06854b9e4b
dwibagus154 <dwibagus154@gmail.com>
2020-11-15 18:53:01 +0700

    menambahkan adt queue dan stackt

---
+++ ADT/Queue/prioqueuechar.c
@@ -0,0 +1,209 @@
+/*
+    Nama : Kadek Dwi Bagus Ananta Udayana 
+    Nim : 13519057 
+    Tanggal : 08 Oktober 2020
+    Topik : Queue
+    Deskripsi : Body prioqueue
+
+*/
+#include "stdio.h"
+#include "boolean.h"
+#include <stdlib.h>
+#include "prioqueuechar.h"
+
+/* ********* Prototype ********* */
+boolean IsEmpty(PrioQueueChar Q)
+/* Mengirim true jika Q kosong: lihat definisi di atas */
+{
+    return ((Head(Q) == Nil) && (Tail(Q) == Nil));
+}
+boolean IsFull(PrioQueueChar Q)
+/* Mengirim true jika tabel penampung elemen Q sudah penuh */
+/* yaitu mengandung elemen sebanyak MaxEl */
+{
+    if (Head(Q) > Tail(Q))
+    {
+        return (Head(Q) - Tail(Q) == 1);
+    }
+    else
+    {
+        return (Tail(Q) - Head(Q) == MaxEl(Q) - 1);
+    }
+}
+int NBElmt(PrioQueueChar Q)
+/* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika Q kosong. */
+{
+    if (Head(Q) == Nil && Tail(Q) == Nil)
+    {
+        return 0;
+    }
+    else
+    {
+        if (Head(Q) <= Tail(Q))
+        {
+            return ((1 + Tail(Q)) - Head(Q));
+        }
+        else
+        {
+            return ((MaxEl(Q) + 1 + Tail(Q)) - Head(Q));
+        }
+    }
+}
+/* *** Kreator *** */
+void MakeEmpty(PrioQueueChar *Q, int Max)
+/* I.S. sembarang */
+/* F.S. Sebuah Q kosong terbentuk dan salah satu kondisi sbb: */
+/* Jika alokasi berhasil, Tabel memori dialokasi berukuran Max */
+/* atau : jika alokasi gagal, Q kosong dg MaxEl=0 */
+/* Proses : Melakukan alokasi, membuat sebuah Q kosong */
+{
+    (*Q).T = (infotype *)malloc((Max + 1) * sizeof(infotype));
+    if ((*Q).T != NULL)
+    {
+        MaxEl(*Q) = Max;
+        Head(*Q) = Nil;
+        Tail(*Q) = Nil;
+    }
+    else /* alokasi gagal */
+    {
+        MaxEl(*Q) = 0;
+    }
+}
+/* *** Destruktor *** */
+void DeAlokasi(PrioQueueChar *Q)
+/* Proses: Mengembalikan memori Q */
+/* I.S. Q pernah dialokasi */
+/* F.S. Q menjadi tidak terdefinisi lagi, MaxEl(Q) diset 0 */
+{
+    MaxEl(*Q) = 0;
+    free((*Q).T);
+}
+/* *** Primitif Add/Delete *** */
+void Enqueue(PrioQueueChar *Q, infotype X)
+/* Proses: Menambahkan X pada Q dengan aturan priority queue, terurut mengecil berdasarkan prio */
+/* I.S. Q mungkin kosong, tabel penampung elemen Q TIDAK penuh */
+/* F.S. X disisipkan pada posisi yang tepat sesuai dengan prioritas,
+        TAIL "maju" dengan mekanisme circular buffer; */
+{
+    int i, a;
+    boolean found;
+    if (IsEmpty(*Q))
+    {
+        Head(*Q) = 0;
+        Tail(*Q) = 0;
+        InfoHead(*Q) = X;
+        InfoTail(*Q) = X;
+    }
+    else
+    {
+        if (Tail(*Q) == MaxEl(*Q) - 1)
+        {
+            Tail(*Q) = 0;
+        }
+        else
+        {
+            Tail(*Q) += 1;
+        }
+        i = Head(*Q);
+        // Cek elmen pertama
+        if (Prio(X) < Prio(Elmt(*Q, i)))
+        {
+            while (i != Tail(*Q))
+            {
+                Elmt(*Q, i + 1) = Elmt(*Q, i);
+                i += 1;
+                if (i == MaxEl(*Q))
+                {
+                    i = 0;
+                }
+            }
+            InfoHead(*Q) = X;
+        }
+        else
+        {
+            found = true;
+            while (found && i != Tail(*Q))
+            {
+                if (Prio(X) < Prio(Elmt(*Q, i)))
+                {
+                    found = false;
+                }
+                else
+                {
+                    i += 1;
+                }
+            }
+            if (!found)
+            {
+                a = i;
+                while (i != Tail(*Q))
+                {
+                    Elmt(*Q, i + 1) = Elmt(*Q, i);
+                    i += 1;
+                    if (i == MaxEl(*Q))
+                    {
+                        i = 0;
+                    }
+                }
+                Elmt(*Q, a) = X;
+            }
+            else
+            {
+                InfoTail(*Q) = X;
+            }
+        }
+    }
+}
+void Dequeue(PrioQueueChar *Q, infotype *X)
+/* Proses: Menghapus X pada Q dengan aturan FIFO */
+/* I.S. Q tidak mungkin kosong */
+/* F.S. X = nilai elemen HEAD pd I.S., HEAD "maju" dengan mekanisme circular buffer;
+        Q mungkin kosong */
+{
+    *X = InfoHead(*Q);
+    if (Head(*Q) == Tail(*Q))
+    { /* Set mjd queue kosong */
+        Head(*Q) = Nil;
+        Tail(*Q) = Nil;
+    }
+    else if (Head(*Q) == (MaxEl(*Q) - 1))
+    {
+        Head(*Q) = 0;
+    }
+    else
+    {
+        Head(*Q) += 1;
+    }
+}
+/* Operasi Tambahan */
+void PrintPrioQueueChar(PrioQueueChar Q)
+/* Mencetak isi queue Q ke layar */
+/* I.S. Q terdefinisi, mungkin kosong */
+/* F.S. Q tercetak ke layar dengan format:
+<prio-1> <elemen-1>
+...
+<prio-n> <elemen-n>
+#
+*/
+{
+    int i = Head(Q);
+    if (IsEmpty(Q))
+    {
+        printf("#\n");
+    }
+    else
+    {
+
+        while (i != Tail(Q))
+        {
+            printf("%d %c\n", Prio(Elmt(Q, i)), Info(Elmt(Q, i)));
+            i += 1;
+            if (i == MaxEl(Q))
+            {
+                i = 0;
+            }
+        }
+        printf("%d %c\n", Prio(Elmt(Q, i)), Info(Elmt(Q, i)));
+        printf("#\n");
+    }
+}
---
+++ ADT/Stackt/stackt.c
@@ -0,0 +1,49 @@
+/* File : stackt.c */
+/* deklarasi stack yang diimplementasi dengan tabel kontigu dan ukuran sama */
+/*
+    Nama = Kadek Dwi Bagus Ananta Udayana 
+    Nim = 13519057
+    Kelas = K1
+    Tanggal = 20 Oktober 2020 
+    Deskripsi = body dari stackt
+*/
+
+#include "stackt.h"
+
+void CreateEmpty(Stack *S)
+/* I.S. sembarang; */
+/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
+/* jadi indeksnya antara 0.. MaxEl */
+/* Ciri stack kosong : TOP bernilai Nil */
+{
+    Top(*S) = Nil;
+}
+/* ************ Predikat Untuk test keadaan KOLEKSI ************ */
+boolean IsEmpty(Stack S)
+/* Mengirim true jika Stack kosong: lihat definisi di atas */
+{
+    return Top(S) == Nil;
+}
+boolean IsFull(Stack S)
+/* Mengirim true jika tabel penampung nilai elemen stack penuh */
+{
+    return (Top(S) == MaxEl - 1);
+}
+/* ************ Menambahkan sebuah elemen ke Stack ************ */
+void Push(Stack *S, infotype X)
+/* Menambahkan X sebagai elemen Stack S. */
+/* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
+/* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
+{
+    Top(*S)++;
+    InfoTop(*S) = X;
+}
+/* ************ Menghapus sebuah elemen Stack ************ */
+void Pop(Stack *S, infotype *X)
+/* Menghapus X dari Stack S. */
+/* I.S. S  tidak mungkin kosong */
+/* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
+{
+    *X = InfoTop(*S);
+    Top(*S)--;
+}
