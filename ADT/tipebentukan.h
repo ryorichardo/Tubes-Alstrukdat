d214c34c0984116cac5c06fdb8f3f53bc81ce522
ryorichardo <37143513+ryorichardo@users.noreply.github.com>
2020-11-18 07:03:13 +0700

    adt point

---
+++ ADT/tipebentukan.h
@@ -0,0 +1,17 @@
+# ifndef tipebentukan_H
+# define tipebentukan_H
+
+typedef struct {
+    char Nama[50];
+    int Harga;
+    int Kapasitas;
+    //Jam Durasi;
+    char Deskripsi[100];
+} Wahana;
+
+typedef struct {
+    char Nama[50];
+    int Harga;
+} Material;
+
+# endif
